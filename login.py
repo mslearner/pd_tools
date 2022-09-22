b55b459d81c160bbbba883f2892c9b6bb80378cc
mslearner <linux_learner@outlook.com>
2022-09-21 17:42:01 -0700

    Added automated way of copying public key

--- login.py
+++ login.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import paramiko
+import io
+import pexpect
 import argparse
 import argparse
 import os
@@ -7,79 +10,72 @@ import sys
 import locale
 import time
 import jwt
+from subprocess import Popen, PIPE, CalledProcessError
+import pexpect
+
 
 def create_jwt(private_key, expiration=60):
-        """
-        Creates a signed JWT, valid for 60 seconds by default.
-        The expiration can be extended beyond this, to a maximum of 600 seconds.
+    now = int(time.time())
+    payload = {
+        "aud": "pdewan-qemu",
+        "sub": "pdewan",
+        "ops": [
+            "shl"
+        ],
+        "exp": now+expiration
+    }
+    encrypted = jwt.encode(
+        payload,
+        key=private_key,
+        algorithm="ES384"
+    )
 
-        :param expiration: int
-        :return:
-        """
-        now = int(time.time())
-        payload = {
-            "aud": "pdewan-qemu",
-            "sub": "pdewan",
-            "ops": [
-                 "shl"
-             ]
-        }
-        encrypted = jwt.encode(
-            payload,
-            key=private_key,
-            algorithm="ES384"
-        )
+    encrypted = encrypted.decode('utf-8')
 
-        
-        encrypted = encrypted.decode('utf-8')
+    return encrypted
 
-        return encrypted
 
-def run_ssh_command(cmd_args, device_hostname, username, token="", ignore_result: bool = False, stdout: bool = True):
+def ssh_copy_id(device_hostname, l_username, token="", ignore_result: bool = False, stdout: bool = True):
+
     template_ssh_args = ["ssh"]
-    template_ssh_args += [f"{username}@{device_hostname}"]
-    result = subprocess.run(template_ssh_args + cmd_args, capture_output = True, encoding = locale.getpreferredencoding(False))
+    template_ssh_args += [f"{l_username}@{device_hostname}"]
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.connect(device_hostname, username=l_username, password=token)
+    stdin, stdout, stderr = client.exec_command('hostname')
+    for line in io.TextIOWrapper(stdout, encoding="utf-8"):
+        print("Logged into the device, hostname="+line)
 
-    if not ignore_result:
-        if result.returncode != 0:
-            printable_args = " ".join(cmd_args)
-            sys.stderr.write(f"error: failed to run: {printable_args}\n")
-            sys.stdout.write(result.stdout)
-            sys.stderr.write(result.stderr)
-            sys.exit(result.returncode)
-        if stdout:
-            return result.stdout
+    key = open(os.path.expanduser('~/.ssh/id_rsa.pub')).read()
+    client.exec_command('echo "%s" > ~/.ssh/authorized_keys' % key)
+    print("Copied the public key to the device --- enjoy!!!!")
 
-#https://stackoverflow.com/questions/19880190/interactive-input-output-using-python
-def provision(device_hostname):
-    device_id= run_ssh_command(["echo","sswws"], device_hostname,"provisioning")
-    print("device_id="+device_id)
-    
+
+# def provision(device_hostname):
+#     print("call server to provision")
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("device_hostname", help="Device hostname or IP address")
-    parser.add_argument("-c", "--disable-strict-host-key-checking",
-                    dest="disable_strict_host_key_checking",
-                    help="Disable strict host key checking in ssh",
-                    default=False,
-                    action="store_true")
-
+    parser.add_argument("device_hostname",
+                        help="Device hostname or IP address")
     args = parser.parse_args()
-    print("provision first!")
+    print("provisioning the device!")
     # provision(args.device_hostname)
     login(args.device_hostname)
-    print("getting token for the id!")
-    print("logging in!")
+
+    print("**Finished**")
+
 
 def login(device_hostname):
     with open('jwt_ec_private.pem', 'r') as file:
         private_key = file.read()
-    jwt=create_jwt(private_key)
+    jwt = create_jwt(private_key)
+    print("Received the token!")
     print("jwt="+jwt)
-    device_id= run_ssh_command(["echo","sswws"], device_hostname,username="restricted-admin",token=jwt)
+    device_id = ssh_copy_id(
+        device_hostname, l_username="restricted-admin", token=jwt)
 
 
 if __name__ == "__main__":
     main()
-
